74edef8f87c691a6d56423fae6c1dc8bdb02a036
FaxMiao <62104212+FaxMiao@users.noreply.github.com>
2022-05-25 14:43:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 //app.js
 App({
+    globalData: {
+        navBarHeight: 0, // 导航栏高度
+        menuRight: 0, // 胶囊距右方间距（方保持左、右间距一致）
+        menuTop: 0, // 胶囊距顶部间距
+        menuHeight: 0, // 胶囊高度（自定义内容可与胶囊高度保证一致）
+    },
     onLaunch: function(options) {
         const that = this;
         // 获取系统信息
@@ -11,15 +17,7 @@ App({
         // 导航栏高度 = 状态栏高度 + 44(所有机型都适用)
         that.globalData.navBarHeight = systemInfo.statusBarHeight + 44;
         that.globalData.menuRight = systemInfo.screenWidth - menuButtonInfo.right;
-        that.globalData.menuBotton = menuButtonInfo.top - systemInfo.statusBarHeight;
+        that.globalData.menuTop = menuButtonInfo.top;
         that.globalData.menuHeight = menuButtonInfo.height;
-    },
-
-    globalData: {
-        navBarHeight: 0, // 导航栏高度
-        menuRight: 0, // 胶囊距右方间距（方保持左、右间距一致）
-        menuBotton: 0, // 胶囊距底部间距（保持底部间距一致）
-        menuHeight: 0, // 胶囊高度（自定义内容可与胶囊高度保证一致）
     }
-
 })
